967e30e4a06fc9817fc1649d2959f1f106047f1e
Patrick McGreevy & Anne Jhetson <student@epicodus.com>
2014-02-24 12:29:48 -0800

    implement webpage functionality

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,18 @@ var Package = {
     return this.length * this.width * this.height;
   }
 };
+
+$(document).ready(function() {
+  $("form#newInput").submit(function(event) {
+    event.preventDefault();
+
+    var aPackage = Object.create(Package);
+    aPackage.weight = parseFloat($("input#weight").val());
+    aPackage.length = parseFloat($("input#length").val());
+    aPackage.width = parseFloat($("input#width").val());
+    aPackage.height = parseFloat($("input#height").val());
+
+    $("p#result").text(aPackage.shippingCost());
+    $("p#result").show();
+  });
+});
